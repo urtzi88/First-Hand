959970a1a3d5c03f23c984d2a9d8be5f9859f8f6
urtzi88 <urtzip@gmail.com>
2016-05-21 22:23:12 +0200

    feedback validation

--- app/assets/javascripts/feedback_sender.js
+++ app/assets/javascripts/feedback_sender.js
@@ -2,7 +2,7 @@ $(function() {
 
     $('.js-service-wrapper').delegate('.js-feedback-provider', 'click', function(event) {
         event.preventDefault();
-        var form = $(event.currentTarget).closest('.card').children('.card-content').children('.content').children('.js-form-container');
+        var form = $(event.currentTarget).closest('.card').find('.js-form-container');
         form.removeClass('is-hidden');
         $(event.currentTarget).addClass('is-hidden');
     });
@@ -10,34 +10,37 @@ $(function() {
     $('.js-service-wrapper').delegate('.js-feedback-form', 'submit', function(event) {
         event.preventDefault();
         var form = event.currentTarget;
-        var rating = $(event.currentTarget).children('.control').children(':checked').val();
-        var feedback = $(event.currentTarget).children('.control').children('textarea').val();
-        var transaction = $(event.currentTarget).children('.control').children('.button').attr('data_transaction');
-        var provider = $(event.currentTarget).children('.control').children('.button').attr('data_provider');
-        console.log(rating);
-        console.log(feedback);
-        updateFeedbackOnTransaction(rating, feedback, transaction, provider, form);
+        var rating = $(form).find(':checked').val();
+        var feedback = $(form).find('textarea').val();
+        var transaction = $(form).find('.button').attr('data_transaction');
+        var provider = $(form).find('.button').attr('data_provider');
+        if(rating == null) {
+            alert("Please input a rating");
+        } else {
+            updateFeedbackOnTransaction(rating, feedback, transaction, provider, form);
+        }
     });
 
     function updateFeedbackOnTransaction(rating, feedback, transaction, user, form) {
         var form = event;
-        var pro_feed = {
+        var details = {
             rating: rating,
             feedback: feedback
         };
         $.ajax({
             method: 'PATCH',
             url: '/users/' + user + '/transactions/' + transaction + '/feedback',
-            data: {rat_feed: pro_feed},
+            data: {rat_feed: details},
             success: updateDomFeedback.bind(form),
         })
     }
 
     function updateDomFeedback(data) {
-
-        $(this.srcElement).addClass('is-hidden');
-        var feedback_btn = $(this.srcElement.parentNode.parentNode.parentNode.parentNode).children('footer').children('.is-item-good');
+        var $form = $(this.srcElement)
+        $form.addClass('is-hidden');
+        var feedback_btn = $form.closest('.is-custom-card').find('.is-item-good');
         feedback_btn.removeClass('is-hidden').addClass('is-disabled');
+        console.log(data);
         var rating = {
             client_rating: data.client_rating,
             provider_rating: data.provider_rating
--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,39 +1,43 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    function slider(slide) {
-        var slide_to = 'slide-' + slide;
-        var button_to = 'button-' + slide;
-        var slides = document.getElementsByClassName('is-slide');
-        for(var i = 0; i < slides.length; i++) {
-            var button_sl = 'button-' + (i + 1);
-            slides[i].classList.add('is-slide-hidden');
-            document.getElementById(button_sl).classList.remove('is-button-active')
+    if (document.getElementsByClassName('is-slider').length > 0) {
+
+        function slider(slide) {
+            var slide_to = 'slide-' + slide;
+            var button_to = 'button-' + slide;
+            var slides = document.getElementsByClassName('is-slide');
+            for(var i = 0; i < slides.length; i++) {
+                var button_sl = 'button-' + (i + 1);
+                slides[i].classList.add('is-slide-hidden');
+                document.getElementById(button_sl).classList.remove('is-button-active')
+            }
+            document.getElementById(slide_to).classList.remove('is-slide-hidden');
+            document.getElementById(button_to).classList.add('is-button-active');
         }
-        document.getElementById(slide_to).classList.remove('is-slide-hidden');
-        document.getElementById(button_to).classList.add('is-button-active');
+
+        document.getElementById('button-1').addEventListener("click", function() {
+            var slide = 1;
+            slider(slide);
+        });
+
+        document.getElementById('button-2').addEventListener("click", function() {
+            var slide = 2;
+            slider(slide);
+        });
+
+        document.getElementById('button-3').addEventListener("click", function() {
+            var slide = 3;
+            slider(slide);
+        });
+
+        var index_slide = 2;
+        var switch_slide = setInterval(function() {
+            var i = index_slide % 3;
+            index_slide += 1;
+            i == 0 ? i = 3 : i;
+            slider(i);
+        }, 5000);
+
     }
-
-    document.getElementById('button-1').addEventListener("click", function() {
-        var slide = 1;
-        slider(slide);
-    });
-
-    document.getElementById('button-2').addEventListener("click", function() {
-        var slide = 2;
-        slider(slide);
-    });
-
-    document.getElementById('button-3').addEventListener("click", function() {
-        var slide = 3;
-        slider(slide);
-    });
-
-    var index_slide = 2;
-    var switch_slide = setInterval(function() {
-        var i = index_slide % 3;
-        index_slide += 1;
-        i == 0 ? i = 3 : i;
-        slider(i);
-    }, 5000);
 
 });
