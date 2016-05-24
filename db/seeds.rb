dd599d1e3d60ce1f43b92b526eb62a9be4ed68cd
urtzi88 <urtzip@gmail.com>
2016-05-24 15:29:44 +0200

    s3 amazon

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,54 +18,54 @@ ld.children.create(name: "Designs")
 car.children.create(name: "Repairs")
 car.children.create(name: "Cleaning")
 
-10.times do |i|
-  fname = Faker::Name::first_name
-  fsurname = Faker::Name::last_name
-  fnumber = Faker::PhoneNumber::phone_number
-  faddress = "Calle Bravo Murillo"
-  fpostcode = ['28015', '28003', '28020'].sample
-  fcity = "Madrid"
-  favatar = Faker::Avatar.image
-
-  Client.create(
-    name: fname,
-    surname: fsurname,
-    email: "#{fname.downcase}@clientmail.com",
-    password: "123456",
-    phone_number: fnumber,
-    address: faddress,
-    postcode: fpostcode,
-    city: fcity,
-    avatar: favatar
-  )
-
-  puts("New client created: Name: " + fname + fsurname + ", #{fname.downcase}@clientmail.com, " + fnumber + faddress + fpostcode + fcity)
-end
-
-10.times do |i|
-  fname = Faker::Name::first_name
-  fsurname = Faker::Name::last_name
-  fnumber = Faker::PhoneNumber::phone_number
-  faddress = "Calle Bravo Murillo"
-  fpostcode = ['28015', '28003', '28020'].sample
-  fcity = "Madrid"
-  service = rand(6..18)
-  price = rand(6..15)
-  favatar = Faker::Avatar.image
-
-  Provider.create(
-    name: fname,
-    surname: fsurname,
-    email: "#{fname.downcase}@providermail.com",
-    password: "123456",
-    phone_number: fnumber,
-    address: faddress,
-    postcode: fpostcode,
-    city: fcity,
-    service_id: service,
-    price_per_hour: price,
-    avatar: favatar
-  )
-
-  puts("New provider created: Name: #{fname} #{fsurname} #{fname.downcase}@providermail.com, #{fnumber} #{faddress} #{fpostcode} #{fcity}, Service: #{service}, Price: #{price}")
-end
+# 10.times do |i|
+#   fname = Faker::Name::first_name
+#   fsurname = Faker::Name::last_name
+#   fnumber = Faker::PhoneNumber::phone_number
+#   faddress = "Calle Bravo Murillo"
+#   fpostcode = ['28015', '28003', '28020'].sample
+#   fcity = "Madrid"
+#   favatar = Faker::Avatar.image
+#
+#   Client.create(
+#     name: fname,
+#     surname: fsurname,
+#     email: "#{fname.downcase}@clientmail.com",
+#     password: "123456",
+#     phone_number: fnumber,
+#     address: faddress,
+#     postcode: fpostcode,
+#     city: fcity,
+#     avatar: favatar
+#   )
+#
+#   puts("New client created: Name: " + fname + fsurname + ", #{fname.downcase}@clientmail.com, " + fnumber + faddress + fpostcode + fcity)
+# end
+#
+# 10.times do |i|
+#   fname = Faker::Name::first_name
+#   fsurname = Faker::Name::last_name
+#   fnumber = Faker::PhoneNumber::phone_number
+#   faddress = "Calle Bravo Murillo"
+#   fpostcode = ['28015', '28003', '28020'].sample
+#   fcity = "Madrid"
+#   service = rand(6..18)
+#   price = rand(6..15)
+#   favatar = Faker::Avatar.image
+#
+#   Provider.create(
+#     name: fname,
+#     surname: fsurname,
+#     email: "#{fname.downcase}@providermail.com",
+#     password: "123456",
+#     phone_number: fnumber,
+#     address: faddress,
+#     postcode: fpostcode,
+#     city: fcity,
+#     service_id: service,
+#     price_per_hour: price,
+#     avatar: favatar
+#   )
+#
+#   puts("New provider created: Name: #{fname} #{fsurname} #{fname.downcase}@providermail.com, #{fnumber} #{faddress} #{fpostcode} #{fcity}, Service: #{service}, Price: #{price}")
+# end
