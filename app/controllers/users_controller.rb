2273d06ccd6c78a49b58eece843b277c1ad03058
urtzi88 <urtzip@gmail.com>
2016-05-25 17:31:28 +0200

    Search fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       @user = current_user
       @transactions = current_user.transactions.order(date_time: :asc)
       if @user.type == "Client"
-        render 'users/client_profile'
+        render 'users/client_search'
       elsif @user.type == "Provider"
         render 'users/provider'
       end
@@ -17,11 +17,11 @@ class UsersController < ApplicationController
     end
   end
 
-  def search
+  def client_profile
     @provider = Provider.new
     @user = current_user
-    @transactions = current_user.transactions.order(id: :asc)
-    render 'users/client_search'
+    @transactions = current_user.transactions.order(date_time: :asc)
+    render 'users/client_profile'
   end
 
   def provider_list
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'users#search'
+  root to: 'users#profile'
   devise_for :users
 
   resources :users, only: [] do
     collection do
-      get :search, to: 'users#search', as: :client_search
+      get :search, to: 'users#client_profile', as: :client_search
       patch '/feedback', to: 'users#update_user_rating'
       get '/provider_list', to: 'users#provider_list'
       get '/provider_list/:service_id', to: 'users#providers_filtered'
