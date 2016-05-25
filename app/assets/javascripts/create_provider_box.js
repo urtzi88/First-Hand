028fa59be68b5368bcbac39f771767adebe0e858
urtzi88 <urtzip@gmail.com>
2016-05-25 16:40:20 +0200

    visual changes on providers cards. Completed list added

--- app/assets/javascripts/create_provider_box.js
+++ app/assets/javascripts/create_provider_box.js
@@ -4,7 +4,7 @@ function createProviderBox(provider) {
         '<div class="media-left"><figure class="image is-128x128">' +
         '<img class="search-avatar-user" src="' + provider.avatar_url + '" alt="avatar"></figure></div>' +
         '<div class="media-content"><div class="content provider-box-content">' +
-        '<h1 class="title is-3">' + provider.name + ' ' + provider.surname + ' ' + provider.average_rating + '(' + provider.rating_amount + ')' + '</h1>' +
+        '<h1 class="title is-3">' + provider.name + ' ' + provider.surname + ' - <span>Rating: ' + provider.average_rating + '(' + provider.rating_amount + ')' + '</span></h1>' +
         '<div class="columns"><div class="column is-half">' +
         '<p><strong>Address: </strong>' + provider.address + '</p>' +
         '<p><strong>Postcode: </strong>' + provider.postcode + '</p>' +
@@ -13,18 +13,16 @@ function createProviderBox(provider) {
         '<p><strong>Service: </strong>' + provider.service.name + '</p>' +
         '<p><strong>Price: </strong>' + provider.price_per_hour + '$ \/ hour</p>' +
         '<p><strong>Previous services: </strong>' + transNumber + '</p>' +
-        '</div></div><div class="columns"><div class="column is-half">' +
-        '<p><strong>Email: </strong>' + provider.email + '</p>' +
-        '<p><strong>Phone Number: </strong>' + provider.phone_number + '</p>' +
-        '</div><div class="column is-half">' +
+        '</div></div><div class="columns"><div class="column is-fullwidth">' +
         '<p><strong>Bio: </strong>' + provider.description + '</p>' +
+        '</div></div><div class="columns"><div class="column is-fullwidth">' +
         '<button class="button js-request-service" data-prov="' + provider.id + '" data-id="">Request service</button>' +
         '<div class="date-and-time is-hidden">' +
         '<div class="user-date">' +
-        '<label for="service_date">Select desired date: </label><br>' +
+        '<label for="service_date">Select date: </label><br>' +
         '<input id="user-date' + provider.id + '" class="is-user-date" type="text" name="service_date">' +
         '</div><div class="user-time">' +
-        '<label for="service_time">Select desired time: </label><br>' +
+        '<label for="service_time">Select time: </label><br>' +
         '<input id="user-time" type="text" name="service_time">' +
         '</div><div class="user-description">' +
         '<label for="service_description">Please input a brief description: </label><br>' +
